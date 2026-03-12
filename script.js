5b5e48a80a9053b6046da424c0a20bce104ee4dc
Siddharthirkal <104129670+Siddharthirkal@users.noreply.github.com>
2026-03-12 16:50:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+const btn = document.getElemenById("btn");
+const output = document.getElementById("output");
+
+function wait(ms){
+	return new Promise(resolve => setTimeout(resolve,ms));
+}
+
+btn.addEventListener("click",async()=>{
+	const text = document.getElementById("text").value;
+	const delay = document.getElementById("delay").value;
+
+	await wait(delay);
+
+	output.textContent = text;
+});
